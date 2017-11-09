a061f4b29daad5eaba4d3d62c1e7ad38cee671e3
rockchen <you@example.com>
2017-11-09 21:03:40 +0800

    update detail to that get the database's date to display the detail page

--- article/views.py
+++ article/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import Article
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello World, Django<h1>")
 
 def detail(request, my_args):
-    return HttpResponse("You're looking at my_args %s." % my_args)
+    post = Article.objects.all()[int(my_args)-1]
+    str = ("title = %s, category = %s, date_time = %s, content = %s" %(post.title, post.category,post.date_time,post.content))
+    return HttpResponse(str)
